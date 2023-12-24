b282024dc5f9b5a0d4c340ccec4c341cb1f11c4c
Danny <dannyhdyt14@gmail.com>
2023-12-24 12:43:58 +0700

    update .feature name

--- src/test/java/com/api/automation/Runner.java
+++ src/test/java/com/api/automation/Runner.java
@@ -7,32 +7,32 @@ public class Runner {
 
     @Karate.Test
     Karate postCreateUser() {
-        return Karate.run("postCreateUser").relativeTo(getClass());
+        return Karate.run("1postCreateUser").relativeTo(getClass());
     }
 
     @Karate.Test
     Karate postCreateUserWithList() {
-        return Karate.run("postCreateUserWithList").relativeTo(getClass());
+        return Karate.run("1postCreateUserWithList").relativeTo(getClass());
     }
 
     @Karate.Test
     Karate getUserNameRequest() {
-        return Karate.run("getUserNameRequest").relativeTo(getClass());
+        return Karate.run("2getUserNameRequest").relativeTo(getClass());
     }
 
     @Karate.Test
     Karate getLoginRequest() {
-        return Karate.run("getLoginRequest").relativeTo(getClass());
+        return Karate.run("2getLoginRequest").relativeTo(getClass());
     }
 
     @Karate.Test
     Karate getLogoutRequest() {
-        return Karate.run("getLogoutRequest").relativeTo(getClass());
+        return Karate.run("2getLogoutRequest").relativeTo(getClass());
     }
 
     @Karate.Test
     Karate putUpdateUser() {
-        return Karate.run("putUpdateUser").relativeTo(getClass());
+        return Karate.run("3putUpdateUser").relativeTo(getClass());
     }
 
     //response 200 ketika data user sebelumnya sudah di create
